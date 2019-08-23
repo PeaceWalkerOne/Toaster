b909e4253ea4790ac28eedcac8a4a8c8f2848d9a
PeaceWalkerOne <54218513+PeaceWalkerOne@users.noreply.github.com>
2019-08-23 17:12:50 -0600

    Actualizacion #2 FINAL
    Fin de a programacion, ahora ya es capaz de calcular tiempo y apagarse por si solo ademas de sentir si se encuentra en un estado de peligro (puerta abierta mientras el horno esta encendido).

--- Toaster_Arduino/Toaster_Arduino.ino
+++ Toaster_Arduino/Toaster_Arduino.ino
@@ -1,12 +1,3 @@
-// This example uses an Adafruit Huzzah ESP8266
-// to connect to shiftr.io.
-//
-// You can check on your device after a successful
-// connection here: https://shiftr.io/try.
-//
-// by Joël Gähwiler
-// https://github.com/256dpi/arduino-mqtt
-
 #include <ESP8266WiFi.h>
 #include <MQTT.h>
 
@@ -17,6 +8,8 @@ int HornoToaster = 12;
 int SensorPuerta = 13;
 int estadoHorno = 0;
 int estadoPuerta = 0;
+long InicioHorneo = 0;
+long TiempoHornear = 1;
 
 WiFiClient net;
 MQTTClient client;
@@ -41,24 +34,56 @@ void connect() {
 
   Serial.println("\nconnected!");
 
-  client.subscribe("/hello");
+  client.subscribe("/toaster/Horno/mensaje");
+  client.subscribe("/toaster/Horno/tiempo");
+
   // client.unsubscribe("/hello");
 }
 
 void messageReceived(String &topic, String &payload) {
   Serial.println("incoming: " + topic + " - " + payload);
+  if (topic == "/toaster/Horno/mensaje") {
+    if (payload == "1") {
+      digitalWrite(HornoToaster , 1);
+      Serial.println("Encender horno");
+      estadoHorno = 1;
+      InicioHorneo = millis();
+    }
+    else {
+      Serial.println("Apagar horno");
+      digitalWrite(HornoToaster , 0);
+      estadoHorno = 0;
+    }
+  }
+  else if (topic == "/toaster/Horno/tiempo") {
+    if (estadoHorno == 0) {
+      client.publish("/toaster/Horno/status", "Apagado");
+    }
+    else {
+      long TiempoElaborado = millis() - InicioHorneo;
+      long Segundos = TiempoElaborado / 1000;
+      long Minutos = Segundos / 60;
+      Segundos = Segundos % 60;
+      String MensajeTiempo = String(int(Minutos));
+      MensajeTiempo = MensajeTiempo + ":";
+      if (Segundos < 10) {
+        MensajeTiempo = MensajeTiempo + "0";
+      }
+      MensajeTiempo = MensajeTiempo + String(int(Segundos));
+      client.publish("/toaster/Horno/status", MensajeTiempo);
+    }
+  }
 }
 
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, pass);
 
-  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
-  // You need to set the IP address directly.
   client.begin("broker.shiftr.io", net);
   client.onMessage(messageReceived);
 
   connect();
+
 }
 
 void loop() {
@@ -69,14 +94,37 @@ void loop() {
     connect();
   }
 
-  // publish a message roughly every second.
+  if (digitalRead(SensorPuerta)) {
+    estadoPuerta = false;
+  }
+  else {
+    estadoPuerta = true;
+  }
+
+
   if (millis() - lastMillis > 1000) {
     String mensaje = String(estadoPuerta);
-    client.publish("/toaster/puerta", mensaje);
+    client.publish("/toaster/puerta/estado", mensaje);
     mensaje = String(estadoHorno);
     client.publish("/toaster/Horno/estado", mensaje);
-    
     lastMillis = millis();
+  }
+
+
+  if (estadoHorno == 1) {
+    long TiempoElaborado = millis() - InicioHorneo;
+    long Segundos = TiempoElaborado / 1000;
+    long Minutos = Segundos / 60;
+    Serial.println(Segundos);
+    //Serial.print("  ");
+    //Serial.print(Minutos);
+    if (Minutos >= TiempoHornear) {
+      Serial.println("Se apago el horno");
+      estadoHorno = 0;
+      digitalWrite(HornoToaster, 0);
+      client.publish("/toaster/Horno/estado", "3");
+    }
 
   }
+
 }
--- Toaster_servidor/index.js
+++ Toaster_servidor/index.js
@@ -1,29 +1,74 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// replace the value below with the Telegram token you receive from @BotFather
+let BrokerMQTT = 'mqtt://broker.shiftr.io';
+let PuertoMQTT = 1883;
+let ClienteIDMQTT = "ServidorToaster";
+let UsuarioMQTT = "PaperBot";
+let ContrasenaMQTT = "MAQUINADEGUERRAESLAONDA";
+let ChatSecretoMQTT = "821249733";
+let EstadodelHorno = false;
 const token = '883954007:AAHf9AIxEPtA9UOy-8WTRXkuwogBdY6EO0I';
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+const bot = new TelegramBot(token, {
+  polling: true
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Vaca XD');
+  let message = msg.text;
+  console.log("el id " + chatId);
+  if (message == "Activar" || message == 'activar' || message == 'A') {
+    client.publish('/toaster/Horno/mensaje', '1');
+    bot.sendMessage(chatId, "Horno Activado");
+    EstadodelHorno = true;
+  } else if (message == "Desactivar" || message == 'desactivar' || message == 'D') {
+    client.publish('/toaster/Horno/mensaje', '0');
+    bot.sendMessage(chatId, "Horno Desactivado");
+    EstadodelHorno = false;
+  } else if (message == "Estado" || message == 'estado' || message == 'E') {
+    client.publish('/toaster/Horno/tiempo', '1');
+    bot.sendMessage(chatId, "Preguntando estado del Horno...");
+  } else {
+    bot.sendMessage(chatId, "Escribe bien que no te entiendo");
+  };
 });
+
+let Opciones = {
+  port: PuertoMQTT,
+  clientId: ClienteIDMQTT,
+  username: UsuarioMQTT,
+  password: ContrasenaMQTT
+}
+
+var mqtt = require('mqtt')
+var client = mqtt.connect(BrokerMQTT, Opciones);
+
+client.on('connect', function() {
+  client.subscribe('/toaster/#', function(err) {
+    console.log("MQTT Activado")
+  })
+})
+
+client.on('message', function(topic, message) {
+  console.log("T: " + topic + "  M: " + message);
+  let Mensaje = message.toString();
+  if (EstadodelHorno) {
+    if (topic == "/toaster/puerta/estado") {
+      if (message == "1") {
+        bot.sendMessage(ChatSecretoMQTT, "Peligro: Puerta Abierta");
+      }
+      else if(message == "3"){
+        EstadodelHorno = false;
+        bot.sendMessage(ChatSecretoMQTT, "Fin del proceso, retirar pagina");
+      }
+    }
+  }
+  if (topic == "/toaster/Horno/status") {
+    if (message == "Apagado") {
+      bot.sendMessage(ChatSecretoMQTT, "Horno apagado");
+    } else {
+      bot.sendMessage(ChatSecretoMQTT, "En proceso desde: " + message + " minutos.");
+    }
+
+  }
+})
